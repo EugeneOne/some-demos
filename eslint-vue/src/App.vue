4e5c6d8383674d80d22d5761d4c6e3b1f0216d76
wanyuqing <wyq_life@yeah.net>
2018-05-27 17:20:33 +0800

    add eslit-test

--- eslint-vue/src/App.vue
+++ eslint-vue/src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img 
+      src="./assets/logo.png" 
+      @click="tagPic">
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'App'
+    name: 'App',
+    methods() {}
 }
 </script>
 
