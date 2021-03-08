80bf1db5724e3407a0c4bb214598ba05fece0197
wanyuqing <wanyuqing@meituan.com>
2021-03-08 19:57:27 +0800

    develop

--- micro-sandbox/SnapshotSandbox.js
+++ micro-sandbox/SnapshotSandbox.js
@@ -2,6 +2,11 @@
  * @Author: wanyuqing
  * @Date: 2021-03-02 20:01:29
  */
+
+const window = {
+  range: 'globe'
+};
+
 function iter(targetObj, callFn) {
   for (const prop in targetObj) {
     if (targetObj.hasOwnProperty(prop)) {
@@ -11,8 +16,8 @@ function iter(targetObj, callFn) {
 }
 
 class SnapshotSandbox {
-  constructor(targetObj) {
-    this.targetObj = targetObj;
+  constructor() {
+    this.targetObj = window;
     // 改动数据
     this.modifySandbox = {};
     // sandbox快照
@@ -21,14 +26,27 @@ class SnapshotSandbox {
   active() {
     // 暂存当前快照
     iter(targetObj, (prop) => {
-      this.targetSnapshotp[prop] = this.targetObj[prop];
+      this.targetSnapshotp[prop] = window[prop];
     });
     // 恢复快照
     Object.keys(this.modifySandbox).forEach((prop) => {
-      this.targetObj;
+      window[prop] = this.modifySandbox[prop];
     });
   }
   inactive() {
     // 记录当前快照
+    this.modifySandbox = {};
+    iter(this.targetSnapshot, props => {
+      if(this.targetSnapshot[props] !== window[prop]) {
+        // 记录变更
+        this.modifySandbox[props] = window[prop];
+        // 全局环境数据
+        window[prop] = this.targetSnapshotp[prop]
+      }
+    })
   }
 }
+
+
+let sandboxWindow = new SnapshotSandbox(window);
+sandboxWindow.range = 'instance'
